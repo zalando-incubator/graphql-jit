64c404d665e4bb154ec34d0bf9d636e5375f2d24
Boopathi Rajaa <me@boopathi.in>
2019-04-16 14:07:30 +0200

    tests for unions

--- src/__tests__/resolve-info.test.ts
+++ src/__tests__/resolve-info.test.ts
@@ -1,10 +1,9 @@
 import { GraphQLSchema, DocumentNode, parse } from "graphql";
-import { GraphQLJitResolveInfo } from "../resolve-info";
 import { compileQuery, isCompiledQuery } from "../execution";
 import { makeExecutableSchema } from "graphql-tools";
 
-describe("resolve-info", () => {
-  describe("GraphQLJitResolveInfo - simple types", () => {
+describe("GraphQLJitResolveInfo", () => {
+  describe("simple types", () => {
     let inf: any;
     const schema = makeExecutableSchema({
       typeDefs: `
@@ -67,7 +66,7 @@ describe("resolve-info", () => {
     });
   });
 
-  describe("jit resolve info for interfaces", () => {
+  describe("interfaces", () => {
     let inf: any;
     const schema = makeExecutableSchema({
       typeDefs: `
@@ -154,6 +153,134 @@ describe("resolve-info", () => {
         Bar2: expect.arrayContaining(["id", "title", "b2"])
       });
     });
+
+    test("fields per type - with fragments", async () => {
+      await executeQuery(
+        schema,
+        parse(
+          `
+            query {
+              iBar {
+                id
+                title
+                ... on Bar1 {
+                  b1
+                  ...bar1
+                }
+                ... on Bar2 {
+                  b2
+                }
+                ...bar2
+              }
+            }
+            fragment bar1 on Bar1 {
+              id
+              b1
+            }
+            fragment bar2 on Bar2 {
+              title
+              b2
+            }
+          `
+        )
+      );
+
+      expect(inf.fields).toMatchObject({
+        IBar: expect.arrayContaining(["id", "title"]),
+        Bar1: expect.arrayContaining(["id", "title", "b1"]),
+        Bar2: expect.arrayContaining(["id", "title", "b2"])
+      });
+    });
+  });
+
+  describe("unions", () => {
+    let inf: any;
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        type Query {
+          uBaz: Baz
+        }
+        union Baz = Foo | Bar
+        type Foo {
+          foo: String
+        }
+        type Bar {
+          bar: Int
+        }
+      `,
+      resolvers: {
+        Query: {
+          uBaz(_: any, _1: any, _2: any, info: any) {
+            inf = info;
+          }
+        },
+        Baz: {
+          __resolveType() {
+            return "Foo";
+          }
+        }
+      }
+    });
+
+    afterEach(() => {
+      inf = undefined;
+    });
+
+    test("union field nodes", async () => {
+      await executeQuery(
+        schema,
+        parse(
+          `
+            query {
+              uBaz {
+                ... on Foo {
+                  foo
+                }
+                ... on Bar {
+                  bar
+                }
+              }
+            }
+          `
+        )
+      );
+
+      expect(inf.fields).toMatchObject({
+        Foo: expect.arrayContaining(["foo"]),
+        Bar: expect.arrayContaining(["bar"])
+      });
+
+      // should not contain the union type as there cannot
+      // be a selection set for unions without a specific type
+      expect(Object.keys(inf.fields)).not.toContain("Baz");
+    });
+
+    test("unions with fragments", async () => {
+      await executeQuery(
+        schema,
+        parse(
+          `
+            query {
+              uBaz {
+                ...foo
+                ...bar
+              }
+            }
+            fragment foo on Foo {
+              foo
+            }
+            fragment bar on Bar {
+              bar
+            }
+          `
+        )
+      );
+
+      expect(inf.fields).toMatchObject({
+        Foo: expect.arrayContaining(["foo"]),
+        Bar: expect.arrayContaining(["bar"])
+      });
+    });
   });
 });
 
@@ -174,8 +301,4 @@ function executeQuery(
     return prepared;
   }
   return prepared.query(rootValue, contextValue, variableValues || {});
-}
-
-interface Resolver {
-  (parent: any, params: any, context: any, info: GraphQLJitResolveInfo): any;
 }
