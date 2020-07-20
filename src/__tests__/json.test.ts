0735170641bea7dd4dca282bc04737e427ccb11e
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-07-19 20:07:57 -0400

    Include use of queryToJSONSchema in test

--- src/__tests__/json.test.ts
+++ src/__tests__/json.test.ts
@@ -1,4 +1,4 @@
-import fastJson from "fast-json-stringify";
+import fastJson, { ObjectSchema, ArraySchema, StringSchema, NumberSchema, BooleanSchema, IntegerSchema, NullSchema } from "fast-json-stringify";
 import {
   formatError,
   GraphQLBoolean,
@@ -10,49 +10,189 @@ import {
   GraphQLSchema,
   GraphQLString,
   parse,
-  GraphQLInt
+  GraphQLInt,
+  getOperationRootType,
+  FieldNode,
+  GraphQLType,
+  isObjectType,
+  isListType,
+  isNonNullType,
+  isEnumType,
+  isScalarType,
+  isAbstractType
 } from "graphql";
-import { buildExecutionContext } from "graphql/execution/execute";
+import { ExecutionContext, collectFields } from "graphql/execution/execute";
 import { compileQuery } from "../index";
+import { resolveFieldDef, collectSubfields } from "../ast";
 
-const responseSchema = {
-  data: {
+const PRIMITIVES: {
+  [key: string]: (
+    | StringSchema
+    | NumberSchema
+    | BooleanSchema
+    | IntegerSchema)["type"];
+} = {
+  Int: "integer",
+  Float: "number",
+  String: "string",
+  Boolean: "boolean",
+  ID: "string"
+};
+
+function queryToJSONSchema(exeContext: ExecutionContext): ObjectSchema {
+  const type = getOperationRootType(exeContext.schema, exeContext.operation);
+  const fields = collectFields(
+    exeContext,
+    type,
+    exeContext.operation.selectionSet,
+    Object.create(null),
+    Object.create(null)
+  );
+  const fieldProperties = Object.create(null);
+  for (const responseName of Object.keys(fields)) {
+    const fieldType = resolveFieldDef(exeContext, type, fields[responseName]);
+    if (!fieldType) {
+      // if field does not exist, it should be ignored for compatibility concerns.
+      // Usually, validation would stop it before getting here but this could be an old query
+      continue;
+    }
+    fieldProperties[responseName] = transformNode(
+      exeContext,
+      fields[responseName],
+      fieldType.type
+    );
+  }
+  return {
     type: "object",
-    nullable: true,
-    additionalProperties: true
-  },
-  errors: {
-    type: "array",
-    items: {
-      type: "object",
-      required: ["message"],
-      properties: {
-        message: { type: "string" },
-        locations: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              line: { type: "integer" },
-              column: { type: "integer" }
+    properties: {
+      data: {
+        type: "object",
+        properties: fieldProperties,
+        nullable: true
+      },
+      errors: {
+        type: "array",
+        items: {
+          type: "object",
+          additionalProperties: true,
+          properties: {
+            message: {
+              type: "string"
+            },
+            path: {
+              type: "array",
+              items: {
+                type: ["string", "number"]
+              }
+            },
+            locations: {
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  line: {
+                    type: "number"
+                  },
+                  column: {
+                    type: "number"
+                  }
+                }
+              }
             }
           }
-        },
-        path: {
-          type: "array",
-          items: { type: "string" }
-        },
-        extensions: {
-          type: "object",
-          properties: {
-            code: { type: "string" }
-          },
-          additionalProperties: true
         }
       }
     }
+  };
+}
+
+function transformNode(
+  exeContext: ExecutionContext,
+  fieldNodes: FieldNode[],
+  type: GraphQLType
+):
+  | ObjectSchema
+  | ArraySchema
+  | StringSchema
+  | NumberSchema
+  | BooleanSchema
+  | IntegerSchema
+  | NullSchema {
+  if (isObjectType(type)) {
+    const subfields = collectSubfields(exeContext, type, fieldNodes);
+    const properties = Object.create(null);
+    for (const responseName of Object.keys(subfields)) {
+      const fieldType = resolveFieldDef(
+        exeContext,
+        type,
+        subfields[responseName]
+      );
+      if (!fieldType) {
+        // if field does not exist, it should be ignored for compatibility concerns.
+        // Usually, validation would stop it before getting here but this could be an old query
+        continue;
+      }
+      properties[responseName] = transformNode(
+        exeContext,
+        subfields[responseName],
+        fieldType.type
+      );
+    }
+    return {
+      type: "object",
+      nullable: true,
+      properties
+    };
   }
-};
+  if (isListType(type)) {
+    return {
+      type: "array",
+      nullable: true,
+      items: transformNode(exeContext, fieldNodes, type.ofType)
+    };
+  }
+  if (isNonNullType(type)) {
+    const nullable = transformNode(exeContext, fieldNodes, type.ofType);
+    if ("nullable" in nullable) {
+      delete nullable.nullable;
+    }
+    return nullable;
+  }
+  if (isEnumType(type)) {
+    return {
+      type: "string",
+      nullable: true
+    };
+  }
+  if (isScalarType(type)) {
+    const jsonSchemaType = PRIMITIVES[type.name];
+    if (jsonSchemaType) {
+      return {
+        type: jsonSchemaType,
+        nullable: true
+      } as StringSchema | NumberSchema | BooleanSchema | IntegerSchema;
+    }
+  }
+  if (isAbstractType(type)) {
+    return exeContext.schema.getPossibleTypes(type).reduce(
+      (res, t) => {
+        const jsonSchema = transformNode(
+          exeContext,
+          fieldNodes,
+          t
+        ) as ObjectSchema;
+        res.properties = { ...res.properties, ...jsonSchema.properties };
+        return res;
+      },
+      {
+        type: "object",
+        nullable: true,
+        properties: {}
+      }
+    );
+  }
+  throw new Error(`Got unhandled type: ${type.name}`);
+}
 
 describe("json schema creator", () => {
   const BlogAuthor = new GraphQLObjectType({
@@ -165,25 +305,11 @@ describe("json schema creator", () => {
       }
     `;
 
-  const context: any = buildExecutionContext(
-    blogSchema,
-    parse(query),
-    null,
-    null,
-    null,
-    null,
-    null
-  );
-
   describe("custom json serializer", () => {
     test("valid response serialization", async () => {
       const prepared: any = compileQuery(blogSchema, parse(query), "", {
-        customJSONSerializer: () => {
-          return fastJson({
-            type: "object",
-            // @ts-ignore
-            properties: responseSchema
-          });
+        customJSONSerializer: (compiledContext) => {
+          return fastJson(queryToJSONSchema(compiledContext));
         }
       });
       const response = await prepared.query(undefined, undefined, {});
@@ -207,12 +333,8 @@ describe("json schema creator", () => {
     });
     test("error response serialization", async () => {
       const prepared: any = compileQuery(blogSchema, parse(query), "", {
-        customJSONSerializer: () => {
-          return fastJson({
-            type: "object",
-            // @ts-ignore
-            properties: responseSchema
-          });
+        customJSONSerializer: (compiledContext) => {
+          return fastJson(queryToJSONSchema(compiledContext));
         }
       });
       const response = {
